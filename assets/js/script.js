649dee31827b51fd8fdcf061dff15757f15e8766
heidiwu3388 <heidiwu3388@yahoo.com>
2022-11-05 21:26:30 -0700

    add initial code for index.html, ideas.html and favorites.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
+// select elements and store them in variables
+let categoryContainerEl = document.querySelector("#category-buttons-container");
 
+
+
+function goToIdeasPage(event){
+    if (event.target.matches("button")){
+        let category = event.target.textContent;
+        document.location.href = `./ideas.html?q=${category}`;
+    }
+}
+
+categoryContainerEl.addEventListener("click", goToIdeasPage);
 
 
 
